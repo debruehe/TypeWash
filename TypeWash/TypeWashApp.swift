d542ec29c1e87b137818fdcf2d0fc443062243ba
Reinhard Thomas <hello@reinhard-thomas.de>
2026-03-11 11:33:18 +0100

    Initial Commit

---
+++ TypeWash/TypeWashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeWashApp.swift
+//  TypeWash
+//
+//  Created by Reinhard Thomas on 11.03.26.
+//
+
+import SwiftUI
+
+@main
+struct TypeWashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
